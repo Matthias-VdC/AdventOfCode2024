ac4c2c371056f89c0c821a89e6893d5b5b0126a3
matthias-vdc <matthias.vdc.be@gmail.com>
2025-11-25 14:06:12 +0100

    Day 3 - Star 1 Complete, first time trying rust

---
+++ day3/star1/notes.md
@@ -0,0 +1,10 @@
+mul(x,y) 1-3 digit numbers
+
+# The following should not work
+mul(4*
+mul(6,9!
+?(12,34)
+mul ( 2 , 4 )
+
+# only 4 things work in this string
+x<b>mul(2,4)</b>%&mul[3,7]!@^do_not_<b>mul(5,5)</b>+mul(32,64]then<b>(mul(11,8)mul(8,5)</b>)
--- day3/star1/src/main.rs
+++ day3/star1/src/main.rs
@@ -1,3 +1,52 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let file = fs::read_to_string("./example")
+        .expect("File not found!");
+    let str_vec: Vec<char> = file.chars().collect();
+    let mut sum: i32 = 0;
+
+    for (i, c) in str_vec.iter().enumerate() {
+        if *c == 'm' && str_vec[i+1] == 'u' && str_vec[i+2] == 'l' && str_vec[i+3] == '(' {
+            // println!("{}", i + 4);
+            let digit_x = verify_digits(&str_vec.clone(), i + 4, ',');
+            // println!("{}", i + 4 + digit_x.0 as usize);
+            let digit_y = verify_digits(&str_vec.clone(), i + 4 + digit_x.0 as usize, ')');
+
+            if digit_x.0 != 0 {
+                if digit_y.0 != 0 {
+                    let x: i32 = digit_x.1.parse::<i32>().unwrap();
+                    let y: i32 = digit_y.1.parse::<i32>().unwrap();
+                    sum += x * y;
+                }
+            }
+        }
+    }
+    println!("{}", sum);
 }
+
+fn verify_digits(str_vec: &Vec<char>, i: usize, delimiter: char) -> (i32, String) {
+    let mut result = String::new();
+
+    if str_vec[i].is_ascii_digit() && str_vec[i+1].is_ascii_digit() && str_vec[i+2].is_ascii_digit() && str_vec[i+3] == delimiter {
+        result.push(str_vec[i]);
+        result.push(str_vec[i+1]);
+        result.push(str_vec[i+2]);
+
+        return (4, result);
+    }
+    if str_vec[i].is_ascii_digit() && str_vec[i+1].is_ascii_digit() && str_vec[i+2] == delimiter {
+        result.push(str_vec[i]);
+        result.push(str_vec[i+1]);
+
+        return (3, result);
+    }
+    if str_vec[i].is_ascii_digit() && str_vec[i+1] == delimiter {
+        result.push(str_vec[i]);
+
+        return (2, result);
+    }
+
+    return (0, result);
+}
+
