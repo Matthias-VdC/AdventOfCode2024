36add19fcbf6f317681ea5b12d1fc44326bc685a
matthias-vdc <matthias.vdc.be@gmail.com>
2025-11-25 20:09:12 +0100

    Figuring out loops

--- day4/star1/app/Main.hs
+++ day4/star1/app/Main.hs
@@ -1,4 +1,14 @@
-module Main where
+import Control.Monad (forM_, when)
+
+
+-- NW -- N -- NE --
+-- -- -- - -- -- --
+-- W  -- - --  E --
+-- -- -- - -- -- --
+-- SW -- S -- SE --
+
+-- https://learnyouahaskell.github.io/chapters.html
+
 
 main :: IO ()
 main = do
@@ -7,12 +17,45 @@ main = do
         let inputLines = lines contents
         let result = length inputLines
 
-        print result
-        -- print inputLines
-        looper 5
+        print ""
 
-looper 0 = return ()
-looper n =
+        looper (0, 0, inputLines)
+
+        -- forM_ (zip [0..] contents) $ \(i, char) -> do -- https://learnxbyexample.com/haskell/for/
+        --     print char
+        --     if char == '\n'
+        --         then row += 1
+        --         else return ()
+
+        --     if char == 'X'
+        --         findX (inputLines, i)
+
+        -- findX (inputLines, index)
+
+
+-- findX ([string], 0) = return ()
+-- findX (str, index) =
+--     do
+--         print str
+--         print index
+
+
+-- checkEast ([string], ()) = return ()
+-- checkEast (str, list) =
+--     do
+--         print str
+--         print list
+
+
+
+looper (0, 0, [_]) = return ()
+looper (row, col, inputLines) =
     do
-        print "loop"
-        looper (n - 1)
+        let line = inputLines !! col
+        let lineLength = length line
+        when (row >= lineLength) $ looper (0, col + 1, inputLines)
+        let currentChar = line !! row
+        print (line, lineLength, currentChar)
+        looper (row + 1, col, inputLines)
+
+
